96ed471226c36637fd038acdb7c1f9d9277866f8
Sailok Chinta <sailokchinta2012@gmail.com>
2020-08-09 18:36:37 +0530

    Added extra checks

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,3 @@
-//their location
-/*var Geo={};
-
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(success,error);
-}else {
-	alert('Geolocation is not supported');
-}
-
-function error() {
-	alert("That's weird! We couldn't find you!");
-}
-
-function success(position) {
-    Geo.lat = position.coords.latitude;
-    Geo.lng = position.coords.longitude;
-    var newrequest= new XMLHttpRequest();
-	newrequest.open("GET","http://api.openweathermap.org/data/2.5/weather?lat="+Geo.lat+"&lon="+Geo.lng+"&appid=0ca412830cb30b338b6dcfae48451f84&units=metric");
-	newrequest.onload=function(){
-		var data=JSON.parse(newrequest.responseText);
-		show(data);console.log(data.name);
-	}
-	newrequest.send();
-}*/
-
 var city=document.getElementById("search");
 var button=document.getElementById("searchbutton");
 
@@ -34,7 +9,8 @@ button.addEventListener("click" , function(){
 		if(request.status==404){
 			window.alert("City not found")
 		}
-		show(data);console.log(data.name);
+		show(data);
+		console.log(data.name);
 	}
 	request.send();
 });
@@ -46,10 +22,32 @@ function show(data){
 	var elem=document.createElement("img");
 	elem.setAttribute("src",url);
 	elem.setAttribute("class","animated fadeInUp");
+	elem.setAttribute("id","icon-img");
+	
+	
+	if(document.getElementById("place").children[0]!=null){
+		document.getElementById("place").children[0].remove();
+	}
+
+	if(document.getElementById("icon-img")!=null){
+		document.getElementById("icon-img").remove();
+	}
+
+	if(document.getElementById("temp").children[1]!=null){
+		document.getElementById("temp").children[1].remove();
+	}
+	
+	if(document.getElementById("wind").children[1]!=null){
+		document.getElementById("wind").children[1].remove();
+	}
+	
+	if(document.getElementById("Weather").children[1]!=null){
+		document.getElementById("Weather").children[1].remove();
+	}
+
 	document.getElementById("place").insertAdjacentHTML('beforeend',"<p class='animated fadeInUp'>"+data.name+"</p>");
 	document.getElementById("temp").insertAdjacentHTML('beforeend',"<h3 class='animated fadeIn'>"+data.main.temp+"&#8451;<br>"+faurenheit+"&#8457;</h3>");
 	document.getElementById("wind").insertAdjacentHTML('beforeend',"<h3 class='animated fadeIn'> Speed:&nbsp;"+data.wind.speed+"&nbsp;mph<br><br>Angle:&nbsp;"+data.wind.deg+"&nbsp;CW</h3>");
 	document.getElementById("Weather").insertAdjacentHTML('beforeend',"<h3 class='animated fadeIn'>"+data.weather[0].main+"</h3>");
 	document.getElementById("icon").appendChild(elem);
 }
-
